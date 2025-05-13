97b950d67b17aa99faef0e1fa52cf084aa4d02da
yonnnxr <carlosborges2007edu@gmail.com>
2025-05-13 13:47:50 -0400

    funcao para transformar mercator

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+function mercatorToLatLng(x, y) {
+  var lon = x / 20037508.34 * 180;
+  var lat = y / 20037508.34 * 180;
+  lat = 180 / Math.PI * (2 * Math.atan(Math.exp(lat * Math.PI / 180)) - Math.PI / 2);
+  return [lat, lon];
+}
+
 var map = L.map('map').setView([-22.2307, -54.8206], 17);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://carto.com/">CartoDB</a>',
+  attribution: '&copy; <a href="https://carto.com/">CartoDB',
   maxZoom: 20
 }).addTo(map);
 
-
-let pontosLayer, redesLayer, quadrasLayer;
+let pontosLayer;
 
 const markers = L.markerClusterGroup({
   iconCreateFunction: function (cluster) {
@@ -23,10 +29,17 @@ const markers = L.markerClusterGroup({
   }
 });
 
-// Pontos
 fetch('https://api-geo-ymve.onrender.com/dados')
   .then(res => res.json())
   .then(data => {
+    // Corrigir todas as coordenadas do GeoJSON para EPSG:4326
+    data.features.forEach(feature => {
+      if (feature.geometry.type === "Point") {
+        const [x, y] = feature.geometry.coordinates;
+        feature.geometry.coordinates = mercatorToLatLng(x, y);
+      }
+    });
+
     pontosLayer = L.geoJSON(data, {
       pointToLayer: (feature, latlng) => L.marker(latlng),
       onEachFeature: (feature, layer) => {
@@ -37,11 +50,12 @@ fetch('https://api-geo-ymve.onrender.com/dados')
         layer.bindPopup(popup);
       }
     });
+
     markers.addLayer(pontosLayer);
     map.addLayer(markers);
   })
   .catch(err => console.error("Erro ao carregar pontos:", err));
-// Toggles
+
 document.getElementById('togglePontos').addEventListener('change', function () {
   this.checked ? map.addLayer(markers) : map.removeLayer(markers);
 });
