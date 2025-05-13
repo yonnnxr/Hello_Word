f40d0715ea399df0fb2e13b2c26cbc6e02c131a4
yonnnxr <carlosborges2007edu@gmail.com>
2025-05-13 14:40:30 -0400

    aviso api ping

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
+// Inicializa o mapa
+var map = L.map('map').setView([-22.2049274382639, -54.8116765895665], 12);
 
-const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Adiciona o mapa base (OpenStreetMap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap',
-  maxZoom: 22
-});
+  maxZoom: 19
+}).addTo(map);
 
-const esriSat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles © Esri',
-  maxZoom: 18
-});
+// Adiciona o mapa de satélite
+var satelliteLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap',
+  maxZoom: 19
+}).addTo(map);
 
-
-const map = L.map('map', {
-  center: [-22.2231, -54.8125],
-  zoom: 13,
-  layers: [osm]
-});
-
-const baseMaps = {
-  "Mapa padrão": osm,
-  "Satélite": esriSat
-};
-L.control.layers(baseMaps).addTo(map);
-
-let pontosLayer;
+// Adiciona um grupo de clusters de marcadores
 const markers = L.markerClusterGroup({
   iconCreateFunction: function (cluster) {
     const count = cluster.getChildCount();
@@ -38,10 +29,15 @@ const markers = L.markerClusterGroup({
   }
 });
 
+// Exibe o indicador de carregamento
+document.getElementById('loadingMessage').style.display = 'block';
+document.getElementById('loadingSpinner').style.display = 'block';
+
+// Carregar os dados da API
 fetch('https://api-geo-ymve.onrender.com/dados')
   .then(res => res.json())
   .then(data => {
-    pontosLayer = L.geoJSON(data, {
+    const pontosLayer = L.geoJSON(data, {
       pointToLayer: (feature, latlng) => L.marker(latlng),
       onEachFeature: (feature, layer) => {
         let popup = '';
@@ -51,13 +47,26 @@ fetch('https://api-geo-ymve.onrender.com/dados')
         layer.bindPopup(popup);
       }
     });
+
     markers.addLayer(pontosLayer);
     map.addLayer(markers);
 
+    // Atualiza o quantitativo de imóveis
     const count = data.features.length;
     document.querySelector('#quantitativo strong').textContent = count;
+
+    // Oculta o indicador de carregamento
+    document.getElementById('loadingMessage').style.display = 'none';
+    document.getElementById('loadingSpinner').style.display = 'none';
   })
-  .catch(err => console.error("Erro ao carregar dados:", err));
+  .catch(err => {
+    alert("Erro ao carregar dados, tente novamente.");
+    document.getElementById('loadingMessage').style.display = 'none';
+    document.getElementById('loadingSpinner').style.display = 'none';
+    console.error("Erro ao carregar pontos:", err);
+  });
+
+// Alternar camadas
 document.getElementById('togglePontos').addEventListener('change', function () {
   this.checked ? map.addLayer(markers) : map.removeLayer(markers);
 });
