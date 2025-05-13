aad74c6755fdeedbd0d39f1fa009cebfcfbb6215
yonnnxr <carlosborges2007edu@gmail.com>
2025-05-13 14:43:28 -0400

    api dormindo

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Inicializa o mapa
+
 var map = L.map('map').setView([-22.2049274382639, -54.8116765895665], 12);
 
-// Adiciona o mapa base (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap',
   maxZoom: 19
 }).addTo(map);
 
-// Adiciona o mapa de satélite
 var satelliteLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap',
   maxZoom: 19
 }).addTo(map);
 
-// Adiciona um grupo de clusters de marcadores
 const markers = L.markerClusterGroup({
   iconCreateFunction: function (cluster) {
     const count = cluster.getChildCount();
@@ -29,11 +26,8 @@ const markers = L.markerClusterGroup({
   }
 });
 
-// Exibe o indicador de carregamento
 document.getElementById('loadingMessage').style.display = 'block';
-document.getElementById('loadingSpinner').style.display = 'block';
 
-// Carregar os dados da API
 fetch('https://api-geo-ymve.onrender.com/dados')
   .then(res => res.json())
   .then(data => {
@@ -51,22 +45,16 @@ fetch('https://api-geo-ymve.onrender.com/dados')
     markers.addLayer(pontosLayer);
     map.addLayer(markers);
 
-    // Atualiza o quantitativo de imóveis
     const count = data.features.length;
     document.querySelector('#quantitativo strong').textContent = count;
 
-    // Oculta o indicador de carregamento
     document.getElementById('loadingMessage').style.display = 'none';
-    document.getElementById('loadingSpinner').style.display = 'none';
   })
   .catch(err => {
     alert("Erro ao carregar dados, tente novamente.");
     document.getElementById('loadingMessage').style.display = 'none';
-    document.getElementById('loadingSpinner').style.display = 'none';
     console.error("Erro ao carregar pontos:", err);
   });
-
-// Alternar camadas
 document.getElementById('togglePontos').addEventListener('change', function () {
   this.checked ? map.addLayer(markers) : map.removeLayer(markers);
 });
