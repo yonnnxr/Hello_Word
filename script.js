e3b355922d57785d82060c991efb47341ddcab89
yonnnxr <carlosborges2007edu@gmail.com>
2025-05-13 15:38:25 -0400

    menu sanduiche mobile

--- script.js
+++ script.js
@@ -70,3 +70,11 @@ fetch('https://api-geo-ymve.onrender.com/dados')
 document.getElementById('togglePontos').addEventListener('change', function () {
   this.checked ? map.addLayer(markers) : map.removeLayer(markers);
 });
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+const mapDiv = document.getElementById('map');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  mapDiv.classList.toggle('sidebar-open');
+});
